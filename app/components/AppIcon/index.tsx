a8e5c1d4bf4af605ee9ace7741b604d233bde387
smhmd <contact.simohamed@gmail.com>
2025-02-09 01:52:16 +0100

    Make navigation bar auto-hide

--- app/components/AppIcon/index.tsx
+++ app/components/AppIcon/index.tsx
@@ -1,4 +1,6 @@
-export function AppIcon({ children, ...props }: React.ComponentProps<'svg'>) {
+type Props = React.ComponentProps<'svg'>
+
+export function AppIcon({ children, ...props }: Props) {
   return (
     <svg
       xmlns='http://www.w3.org/2000/svg'
