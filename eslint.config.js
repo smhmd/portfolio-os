ce7f565b4483467c7d1ba1bc0fd95f98799c7428
smhmd <contact.simohamed@gmail.com>
2025-02-09 01:41:56 +0100

    Remove eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -51,7 +51,7 @@ const config = [
       '@typescript-eslint/no-unused-vars': 'warn',
       'react/react-in-jsx-scope': 'off',
       'prettier/prettier': 'warn',
-      'arrow-body-style': 'warn',
+      'arrow-body-style': 'off',
       'prefer-arrow-callback': 'warn',
     },
   },
