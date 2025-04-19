85eb79f80ca9d8b26ddc4e14e41bc40457fd79c4
smhmd <contact.simohamed@gmail.com>
2025-04-19 17:14:32 +0100

    add initial 404 page

--- app/lib/constants.ts
+++ app/lib/constants.ts
@@ -1,1 +1,3 @@
 export const SHOW_TAILWIND_BREAKPOINTS = false
+export const GITHUB_REPO = 'smhmd/portfolio-os'
+export const REPO_LINK = `https://github.com/${GITHUB_REPO}/`
--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,5 @@
 import {
+  type ErrorResponse,
   isRouteErrorResponse,
   Links,
   Meta,
@@ -58,30 +59,21 @@ export default function App() {
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
-  let message = 'Oops!'
-  let details = 'An unexpected error occurred.'
-  let stack: string | undefined
-
-  if (isRouteErrorResponse(error)) {
-    message = error.status === 404 ? '404' : 'Error'
-    details =
-      error.status === 404
-        ? 'The requested page could not be found.'
-        : error.statusText || details
-  } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message
-    stack = error.stack
+  let e: ErrorResponse = {
+    status: 499,
+    statusText: 'CLIENT_CRASHED',
+    data: undefined,
   }
 
-  return (
-    <main className='container mx-auto p-4 pt-16'>
-      <h1>{message}</h1>
-      <p>{details}</p>
-      {stack && (
-        <pre className='w-full overflow-x-auto p-4'>
-          <code>{stack}</code>
-        </pre>
-      )}
-    </main>
-  )
+  if (isRouteErrorResponse(error)) {
+    e = error
+  } else if (error instanceof Error) {
+    e = {
+      status: 499,
+      statusText: error.message,
+      data: error.stack,
+    }
+  }
+
+  return <BSOD error={e} />
 }
