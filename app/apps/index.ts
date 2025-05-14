3e930dbfd84fb517de9660c90b51c75962879954
smhmd <contact.simohamed@gmail.com>
2025-05-14 16:14:30 +0100

    Remove undeveloped apps (radio, camera, files, gallery)

--- app/apps/index.ts
+++ app/apps/index.ts
@@ -1,18 +1,10 @@
 import { metadata as _2048 } from './2048/metadata'
-import { metadata as camera } from './camera/metadata'
 import { metadata as diceRoll } from './dice-roll/metadata'
 import { metadata as drumMachine } from './drum-machine/metadata'
-import { metadata as files } from './files/metadata'
-import { metadata as gallery } from './gallery/metadata'
 import { metadata as magnetize } from './magnetize/metadata'
-import { metadata as radio } from './radio/metadata'
 import { metadata as typingTest } from './typing-test/metadata'
 
 export const apps = {
-  camera,
-  files,
-  gallery,
-  radio,
   '2048': _2048,
   'typing-test': typingTest,
   'drum-machine': drumMachine,
